3bc92313e832aafff816cb473910f80df86fdeee
Hristo Vladev <hvladev@funkyparentheses.com>
2021-03-31 22:54:05 +0300

    fish: disable spring; use asdf; add more abbreviations

--- fish/config.fish
+++ fish/config.fish
@@ -11,7 +11,11 @@ set -g __fish_git_prompt_char_stateseparator ' '
 source /usr/local/share/chruby/chruby.fish
 source /usr/local/share/chruby/auto.fish
 
+# Configure asdf
+source /usr/local/opt/asdf/asdf.fish
+
 set -x EDITOR nvim
+set -x DISABLE_SPRING 1
 
 # GPG
 set -x GPG_TTY (tty)
@@ -42,13 +46,18 @@ abbr --add --global e $EDITOR
 
 abbr --add --global chkx tmuxinator start checkoutx
 
+abbr --add --global g git
 abbr --add --global gs git status
 abbr --add --global gd git diff
 abbr --add --global gds git diff --staged
 abbr --add --global ga git add
 abbr --add --global gc git commit
+abbr --add --global gcm git commit -m
 abbr --add --global gl git log --oneline --graph
 abbr --add --global gco git checkout
 abbr --add --global gfm git fetch origin master --prune
 abbr --add --global gpm git pull origin master --prune
 abbr --add --global grm git rebase -i --autosquash origin/master
+
+abbr --add --global h heroku
+abbr --add --global hr heroku run
